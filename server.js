b695d2656a8c93f676ae6ab626837ad46287a2de
Atakan Savaş <a.savas@hotmail.com.tr>
2017-11-19 13:03:24 +0300

    initial

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(req.url);
     var queryString = url.parse(req.url, true).query;
-    res.write("<br />");◘
-    res.write("<br />");◘
+    res.write("<br />");
+    res.write("<br />");
     
     //Calling a method from extended class.
     //res.write("The date and time are currently: " + utility.myDateTime());
